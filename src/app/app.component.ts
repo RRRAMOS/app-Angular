573571751456a0b7222e693f1ea73a57d7019c33
RRRAMOS <dsraquelramos@gmail.com>
2021-01-29 23:22:43 -0300

    feat:primeiros componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // forma de chamar o componente- default
   selector: 'app-root',
+  // template
   templateUrl: './app.component.html',
+  // Estilo
   styleUrls: ['./app.component.scss']
 })
+
+// componente e classes: podem  conter funções, variaveis, outras classes ou outros métodos
 export class AppComponent {
-  title = 'appangular';
+  //interpolação
+constructor(){}
 }
